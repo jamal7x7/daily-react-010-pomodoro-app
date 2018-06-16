17a25c9abfc8263dcf935d0c03d2fcf67dd28d6e
jamal7X7 <jamal7x7@gmail.com>
2018-06-16 06:53:23 +0000

    grid

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,44 @@ import '../styles/App.css'
 
 const Header = () => (
   <header className='App-header'>
-    <h1 className='App-title'>Pomodoro</h1>
+    <h1 className='App-title'>Pomodoro </h1>
   </header>
+)
+
+const Audio = (props) => (
+  <div className='audio'>
+    Audio
+  </div>
+)
+const Duration = (props) => (
+  <div className='duration'>
+    Duration
+  </div>
+)
+const Timer = (props) => (
+  <div className='timer'>
+    Timer
+  </div>
+)
+const Play = (props) => (
+  <div className='play'>
+    Play
+  </div>
+)
+const Progression = (props) => (
+  <div className='progression'>
+    P
+  </div>
+)
+
+const Pomodoro = (props) => (
+  <div className='pomodoro'>
+    <Audio />
+    <Duration />
+    <Timer />
+    <Play />
+    <Progression />
+  </div>
 )
 
 class App extends Component {
@@ -12,6 +48,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Header />
+        <Pomodoro />
 
       </div>
     )
